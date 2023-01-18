e1a75a95a5b2635131a271867120c09def1a5ed2
John Hawley <21johnh21@gmail.com>
2023-01-17 19:51:43 -0500

    Initial Commit

---
+++ git_test_2/git_test_2App.swift
@@ -0,0 +1,17 @@
+//
+//  git_test_2App.swift
+//  git_test_2
+//
+//  Created by John Hawley on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct git_test_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
